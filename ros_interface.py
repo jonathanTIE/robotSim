162eecf1348acc89d48e83259d1ccce9940c0763
jonathanTIE <jonathan_bleu@hotmail.fr>
2021-07-23 00:38:36 +0200

    ²

--- ros_interface.py
+++ ros_interface.py
@@ -9,14 +9,19 @@ class RosInterface(Node):
     def __init__(self, node_name = "robotSim"):
         super().__init__(node_name)
 
-    def update_speed_reading_rate(self, rate = 0): #if 0 : stop publishing
-        self.publisher_speed = self.create_publisher(type, 'speed_reading', 10) #le 10 est arbitraire et fait référence à un "QoS setting", voir ici https://docs.ros2.org/foxy/api/rclpy/api/node.html#rclpy.node.Node.create_publisher
-        self.speedTimer = self.create_timer(rate, self.publish_speed_reading)
+    def update_data_continuous(self, name, type, get_data_callback, rate):
+        self.publisher_speed = self.create_publisher(type, name, 10) #le 10 est arbitraire et fait référence à un "QoS setting", voir ici https://docs.ros2.org/foxy/api/rclpy/api/node.html#rclpy.node.Node.create_publisher
+        self.speedTimer = self.create_timer(rate, self.get_data_callback)
 
-    def publish_speed_reading(self):
-        self.publisher_speed.publish(data)
+    def publish_data(self, get_data_callback):
+        self.publisher_speed.publish(get_data_callback())
 
-
+    def register_msg_cb(self, cb, msg_type):
+        #self.[[DYNAMICNAME]] = self.create_subscription(
+    # msg_type,
+    # 'name_of_topic',
+    #cb,
+    # 10)
 
     """
     Recevoir dans un action si on doit déclencher ou stopper ou changer le rate 
