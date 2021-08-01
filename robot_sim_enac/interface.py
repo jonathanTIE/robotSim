b44135cc5bd4a5f1704afb92c42b77260a856910
jonathanTIE <jonathan_bleu@hotmail.fr>
2021-08-01 23:46:36 +0200

    cleaning directories

---
+++ robot_sim_enac/interface.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+from actuators import Actuators, RWType
+
+SPEED_REG = "SpeedCmd {} (.+) (.+) (.+)"
+POS_REG = "PosCmd {} (.+) (.+)"
+POS_ORIENT_REG = "PosCmdOrient {} (.+) (.+) (.+)"
+ACTUATOR_CMD = "ActuatorCmd {} (.+) (.+)"
+ACTUATORS_REQUEST = "ActuatorsRequest {}"
+
+ACTUATOR_DECL = "ActuatorDecl {} {} {} {} {} {} {}"
+POS_REPORT = "PosReport {} {} {} {}"
+ACTUATOR_REPORT = "ActuatorReport {} {} {}"
+
+KILL_CMD = "Shutdown {}"
+
+
+class Interface():
+    """
+        Abstract class that needs to be inherited with any "outside" connection/interface (Serial, Ivy,..)
+        in order to communicate with the simulator
+    """
+    def __init__(self, robot_name):
+        pass
+
+    
+    def start(self, *args):
+        raise NotImplementedError()
+
+    def process_com(self):
+        raise NotImplementedError()
+
+    def stop(self):
+        raise NotImplementedError()
+
+    def update_data_continuous(self, name : str, type_msg_str: str, get_data_callback, rate : float):
+        """
+        Send continuously(at a set rate) a certain data supported (string, twist,...)
+        :param name: name of the parameter
+        :param type_msg_str: type of the msg, in str ("string", "twist", ...)
+        :param get_data_callback: function from the simulator, called to get the latest data
+        :param rate: rate in time per seconds
+        :return:
+        """
+        raise NotImplementedError()
+    def register_msg_callback(self, callback, service_type):
+        """
+        Function to call preferably when initialising the simulator,
+        a callback function from the simulator is called with the arguments provided from the interface depending on the msg type
+        exemple : update_speed, SpeedCmd
+        :param callback:
+        :param service_type:
+        :return:
+        """
+        raise NotImplementedError()
+
+
---
+++ robot_sim_enac/simu_robot.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+import time
+from enum import Enum
+import sys
+from robot_sim_enac.navigation import Navigation
+from robot_sim_enac.actuators import Actuators
+#import messages_pb2 as m
+from robot_sim_enac.ros_interface import RosInterface
+import rclpy
+
+from geometry_msgs.msg import Twist
+
+BUS = "127.255.255.255:2010"
+
+
+class Robot:
+    
+    class Modules(Enum):
+        NAV = 0
+        #ODOM_REPORT = 1
+        ACTUATORS = 1
+
+    def __init__(self, robot_name, bus=BUS, pos_init=(1500, 1000, 0)):
+        self.actuators = Actuators()
+        #self.com = IvyInterface(robot_name, self.actuators, bus)
+        self.nav = Navigation(pos_init)
+        self.modules_period = {}
+        self.modules_update = {}
+        self.modules_update_time = {}
+        self.register_module(Robot.Modules.NAV, 0.05, self.nav.update)
+        self.register_module(Robot.Modules.ACTUATORS, 1, self.update_actuators)
+
+        #COMMUNICATION INIT :
+        self.com = RosInterface("robotSim")
+        self.com.start()
+        self.com.register_msg_callback('speed_cmd', Twist, self.nav.set_speed)
+        self.com.register_msg_callback('position_cmd', Twist, self.nav.set_pos_objective)
+        #self.com.register_msg_callback('actuator_cmd', self.actuators.handle_cmd, Actuators)
+
+        self.com.update_data_continuous("odom_report", Twist, self.get_odom_report, 0.1)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, t, value, traceback):
+        if self.com.running:
+            self.com.stop()
+    
+    def register_module(self, module, period, update):
+        self.modules_period[module] = period
+        self.modules_update[module] = update
+        self.modules_update_time[module] = time.time()
+    
+    def update(self):
+        for module in Robot.Modules:
+            now = time.time()
+            dt = now - self.modules_update_time[module]
+            if dt >= self.modules_period[module]:
+                self.modules_update[module](dt)
+                self.modules_update_time[module] = now
+
+
+    def update_actuators(self, dt):
+        self.actuators.update()
+        for ac in self.actuators.actuators:
+            if ac.val_changed == True:
+                ac.val_changed=False
+                #self.com.report_actuator(ac)
+
+    def get_odom_report(self):
+        x, y, theta = self.nav.pos
+        return [x,y,theta]
+        
+    def run(self):
+        while self.com:#.running :
+            self.update()
+            self.com.process_com()
+            time.sleep(0.01)
+        #self.com.stop ()
+
+
+def main(args=None):
+    robot = Robot("robot sim")
+    while True:
+        robot.run()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: ./simu_robot.py robot_name")
+        exit(-1)
+    with Robot(sys.argv[1]) as robot:
+        robot.run()
